6108d5d120b6dd325a7f44eeaefb47110e49bb7b
curryyou <CareYou@youtaehoui-macbook-pro.local>
2016-06-17 15:58:12 +0900

    Initial Commit

---
+++ FoodDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodDiary
+//
+//  Created by YouTaeho on 2016. 6. 17..
+//  Copyright © 2016년 curryyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
