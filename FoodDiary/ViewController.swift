c7155a1f4cea28b7d03152f1f4c4b2bfa3dcf5b7
curryyou <CareYou@YouTaehoui-MacBook-Pro.local>
2016-06-17 17:08:55 +0900

    라벨을 생성하여 띄운다.

--- FoodDiary/ViewController.swift
+++ FoodDiary/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func changeLabel(sender: AnyObject){
+        let label = UILabel(frame: CGRectMake(30, 30, 100, 50))
+        label.text = "hello world"
+        self.view.addSubview(label)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
